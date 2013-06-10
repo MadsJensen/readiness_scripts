ea281e1af96a4af6682fbd432c49d897deda5b3e
Mads Jensen <mje.mads@gmail.com>
2013-06-10 15:42:05 +0200

    updated saveBadChans.py

--- saveBadChans.py
+++ saveBadChans.py
@@ -6,14 +6,25 @@ Created on Mon Jun  3 15:08:40 2013
 """
 
 from mne import fiff
+import csv
 
 
-def saveBadChns(sub_id, session):
+def save_bad_chans(sub_id, session):
+    """
+    function to extract the bad channel number from a fif file and save them as a  
+    """
     raw_fname = "sub_" + str(sub_id) + "_%s_raw.fif" % session
-    badChns_fname = "sub_" + str(sub_id) + "_%s_badChans.txt" % session
-    f = open(badChns_fname, 'w')
-
+    bad_chans_fname = "sub_" + str(sub_id) + "_%s_badChans.csv" % session
+    
     raw = fiff.Raw(raw_fname, preload=False)
-    badChans = raw.info['bads']
-    for bc in badChans:
-        f.write("%s\n" % bc)
+    bad_chans = raw.info['bads']
+
+    bc_list = [0]
+    for i in range(len(bad_chans)):
+        bc_list.append(bad_chans[i][-4:])
+    csv_writer = csv.writer(open(bad_chans_fname , 'wb'), delimiter=',').writerow           
+    csv_writer(bc_list)                                                      
+
+
+#    for bc in foo:
+#        f.write("%s\n" % bc)
