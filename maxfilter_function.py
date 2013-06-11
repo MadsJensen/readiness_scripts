d57b47262fa4e10642fd1e34763996cdb0e7f9c4
Mads Jensen <mje.mads@gmail.com>
2013-06-11 13:00:28 +0200

    updated maxfilter function

--- maxfilter_function.py
+++ maxfilter_function.py
@@ -18,12 +18,16 @@ def mfFunction(sub_id, session):
     # setup the badchannels
     badchannel_filename = 'sub_' + str(sub_id) + '_%s_badChans.csv' % session
     bcFname = badchannel_path + badchannel_filename
+    badchannels = list(csv.reader(bcFname))
+    
+    print badchannels
     # badchannels = np.genfromtxt(bcFname, delimiter=
     #
     #  apply maxfilter to correct for bad channels, movement & tsss
-     !/neuro/bin/util/maxfilter -f {raw_fname} -o {tsss_fname} -st 10 -movecomp -hp {headpos_logname} -bad {badchannels} -v -force | tee {tsss_logname}
-+    # 
+#    !/neuro/bin/util/maxfilter -f {raw_fname} -o {tsss_fname} -st 10 -movecomp -hp {headpos_logname} -bad {badchannels} -v -force | tee {tsss_logname}    # 
     # apply maxfilter to make the transformation to 0,0,0
 #    !/neuro/bin/util/maxfilter -f {tsss_fname} -o {trans_fname} -trans default -force -v | tee {trans_logname}
     
+
+
 
