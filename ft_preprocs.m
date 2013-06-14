fe1042b4f983cd131110d97f32e65d28b2d0c068
Mads Jensen <mje.mads@gmail.com>
2013-06-14 23:41:22 +0200

    updated ft_preproc.m

--- ft_preprocs.m
+++ ft_preprocs.m
@@ -1,8 +1,7 @@
 
 
 cfg                         = [];
-cfg.dataset                 = ''
-cfg.header.file             = ''
+cfg.dataset                 = 'sub_1_interupt_tsss_mc.fif';
 cfg.trialdef.eventtype      = 'STI101';
 cfg.trialdef.eventvalue     = 1; % trigger value
 cfg.trialdef.prestim        = 3.5; % in seconds
@@ -10,6 +9,7 @@ cfg.trialdef.poststim       = .5; % in seconds
 
 cfg = ft_definetrial(cfg);
 
+cfg.trl([1,3,6,8,18,28,30,34,36,37,41,43,44,47,48,50,57,58,62,65;],:) =[];
 
 cfg.channel            = {'MEGGRAD'};
 cfg.bsfilter                = 'yes';
@@ -29,23 +29,24 @@ cfg.detrend                 = 'no'
 
 data_GRAD = ft_resampledata(cfg, data_GRAD);
 
-%
+%%
 
 
-% cfg          = [];
-% cfg.method   = 'summary';
-% cfg.alim     = 1e-12; 
-% cfg.megscale = 1;
-% cfg.eogscale = 5e-8; 
-% data_GRAD_vis        = ft_rejectvisual(cfg,data_GRAD); 
+cfg          = [];
 
-%
+cfg.method   = 'summary';
+cfg.alim     = 1e-12; 
+cfg.megscale = 1;
+cfg.eogscale = 5e-8; 
+data_GRAD_vis        = ft_rejectvisual(cfg,data_GRAD); 
+
+%%
 % ICA
 cfg        = [];
 cfg.runica.pca = 64;
 cfg.channel = 'MEGGRAD';
 cfg.method = 'runica'; % this is the default and uses the implementation from EEGLAB
-data_GRAD_comp = ft_componentanalysis(cfg, data_GRAD);
+data_GRAD_comp = ft_componentanalysis(cfg, data_GRAD_vis);
 
 %
 % plot the components for visual inspection
@@ -64,19 +65,19 @@ ft_databrowser(cfg, data_GRAD_comp)
 %
 %%
 cfg = [];
-cfg.component = [1,4]; % to be removed component(s)%
-ses1_GRAD_ica = ft_rejectcomponent(cfg, data_GRAD_comp, data_GRAD)
+cfg.component = [1,4,9]; % to be removed component(s)%
+GRAD_ica = ft_rejectcomponent(cfg, data_GRAD_comp, data_GRAD)
 
 %
 
-save ses1_GRAD_ica_ft ses1_GRAD_ica
+save sub_1_interupt_ica GRAD_ica
 
 %%
 
 cfg = [];
 cfg.keeptrials = 'yes';
 
-data_GRAD_ica_avg = ft_timelockanalysis(cfg, data_ica)
+data_GRAD_ica_avg = ft_timelockanalysis(cfg, GRAD_ica)
 
 
 %%
