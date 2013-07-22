e42c86e5c714394e5574552a73b4f09e0664dbc3
Mads Jensen <mje.mads@gmail.com>
2013-07-22 13:58:46 +0200

    update fs_mkdir

--- fs_mkdirs.sh
+++ fs_mkdirs.sh
@@ -1,8 +1,8 @@
 
 
 # write the subject numbers needed after the "in"
-for i in 1 6 3
+for i in 1 2 3
 do
     fname=$(printf 'fs_sub_%d'  $i) 
-    echo $fname
+    mksubjdirs $SUBJECTS_DIR/$fname
 done
