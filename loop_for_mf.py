d6363c398f450a0e63cfe983adef7127e0f9e1e8
Mads Jensen <mje.mads@gmail.com>
2013-08-01 11:58:24 +0200

    updated to use

--- loop_for_mf.py
+++ loop_for_mf.py
@@ -1,8 +1,8 @@
 
 
-from mne.preprocessing import maxfilter_mj
-
-#### Select conditions and subjects numbers to maxfilter
+import csv
+from mne.preprocessing.maxfilter_mj import apply_maxfilter
+#### Select (sub, condition)s and subjects numbers to maxfilter
 
 conditions = ["plan"]
 subs = [2]
@@ -13,43 +13,43 @@ ctc  = "/projects/MINDLAB2011_24-MEG-readiness/misc/ct_sparse_Mar11-May13.fif "
 
 for condition in conditions:
     for sub in subs:
-        raw_fname = "sub_" + str(sub_id) + "_%s_raw.fif" % session
-        tsss_fname = "sub_" + str(sub_id) + "_%s_tsss_mc.fif" % session
-        tsss_logname = "sub_" + str(sub_id) + "_%s_tsss_mc.log" % session
-        trans_fname = "sub_" + str(sub_id) + "_%s_tsss_mc_trans.fif" % session
-        trans_logname = "sub_" + str(sub_id) + "_%s_tsss_mc_trans.log" % session
-        headpos_logname = "sub_" + str(sub_id) + "_%s_headpos.txt" % session
+        raw_fname = "sub_%d_%s_raw.fif" % (sub, condition)
+        tsss_fname = "sub_%d_%s_tsss_mc.fif" % (sub, condition)
+        tsss_logname = "sub_%d_%s_tsss_mc.log" % (sub, condition)
+        trans_fname = "sub_%d_%s_tsss_mc_trans.fif" % (sub, condition)
+        trans_logname = "sub_%d_%s_tsss_mc_trans.log" % (sub, condition)
+        headpos_logname = "sub_%d_%s_headpos.txt " % (sub, condition)
         
 
-        badchannel_path = project_path + "/projects/MINDLAB2011_24-MEG-readiness/misc/Badchannels/"
-        badchannel_filename = 'sub_' + str(sub_id) + '_%s_badChans.csv' % session
+        badchannel_path = "/projects/MINDLAB2011_24-MEG-readiness/misc/Badchannels/"
+        badchannel_filename = 'sub_%d_%s_badChans.csv' % (sub, condition)
         bcFname = badchannel_path + badchannel_filename
         badchannels = list(csv.reader(open(bcFname)))
 
         apply_maxfilter(raw_fname, 
                 tsss_fname,
-                origin=None 
-                frame='device'
-                bad='%s' %badchannels
-                autobad='off'
-                skip=None
-                force=False
+                origin=None,
+                frame='head',
+                bad='%s' %badchannels,
+                autobad='off',
+                skip=None,
+                force=False,
                 st=True, 
-                st_buflen=10.
-                st_corr=0.980 
-                mv_trans=None
-                mv_comp=True
-                mv_headpos=True
-                mv_hp=headpos_logname
-                mv_hpistep=None
-                mv_hpisubt=None
-                mv_hpicons=True
-                linefreq=None
-                mx_args='| tee %s ' % tsss_logname
-                overwrite=True
-                verbose=True
-                cal=cal
-                ctc=ctc
+                st_buflen=10.,
+                st_corr=0.980 ,
+                mv_trans=None,
+                mv_comp=True,
+                mv_headpos=False,
+                mv_hp='%s' % headpos_logname, 
+                mv_hpistep=None,
+                mv_hpisubt=None,
+                mv_hpicons=True,
+                linefreq=None,
+                mx_args='-v | tee %s ' % tsss_logname,
+                overwrite=True,
+                verbose=True,
+                cal=cal,
+                ctc=ctc,
                 )
 
      
