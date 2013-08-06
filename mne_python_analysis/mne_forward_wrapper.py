b4ff8a09d0799627c0b04f99545241bc3d9d9c5b
Mads Jensen <mje.mads@gmail.com>
2013-08-06 13:06:00 +0200

    small bugsfixes

--- mne_python_analysis/mne_forward_wrapper.py
+++ mne_python_analysis/mne_forward_wrapper.py
@@ -2,10 +2,13 @@
 
 
 import os
-
+import logging
+logger = logging.getLogger('mne')
 
 def mne_forward_wrapper(sub_id, session, ico=4):
-
+    """ calls mne_setup_forward_model
+    """    
+    fname = "sub_%d_%s_tsss_mc.fif"    
 
     cmd = ("mne_setup_forward_model --subject %s --surf --homog --ico %d " 
             % (fname, ico))
