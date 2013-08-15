3f4b8cab8c660968b322be5d96cefedd6ccb06a9
Mads Jensen <mje.mads@gmail.com>
2013-08-16 00:29:01 +0200

    updated smoothiles

--- smooth_files.m
+++ smooth_files.m
@@ -1,9 +1,9 @@
 function smooth_files
-fileList = dir('t*.img');
+fileList = rdir('./**/*.img');
 
 for j = 1:length(fileList)
   P = fileList(j).name;
-  Q = ['s', fileList(j).name];
+  Q =  [fileList(j).name(1:end-13), 'strial0001.img'];
   S = [5 5 10];
 
   fprintf('smoothing: %d of %d\n', j, length(fileList))
