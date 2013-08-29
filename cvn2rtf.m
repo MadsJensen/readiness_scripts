34e9fe20613ad5dd72f19e252c10b9a84237d37a
Mads Jensen <mje.mads@gmail.com>
2013-08-29 11:34:45 +0200

    made to a function

--- cvn2rtf.m
+++ cvn2rtf.m
@@ -1,29 +1,28 @@
+function cvn2rtf(sub_id, session)
+
+
+fname = sprintf('sub_%d_%s_tsss_mc_preproc_ica.fif', sub_id, session);
+oname = ['spmeeg_', fname];
+
 spm('defaults', 'eeg');
 
 S = [];
-S.dataset = '/media/mje/KINGSTON/MEG_libet/Data/sub_2_plan_tsss_mc_preproc_ica.fif';
+S.dataset = ['../', fname];
 S.mode = 'continuous';
 S.channels = {'MEGPLANAR'};
 S.eventpadding = 0;
 S.blocksize = 3276800;
 S.checkboundary = 1;
 S.saveorigheader = 1;
-S.outfile = 'spmeeg_sub_2_plan_tsss_mc_preproc_ica';
+S.outfile = oname;
 S.timewin = [];
-S.conditionlabels = {'Undefined'};
+S.conditionlabels = {session};
 S.inputformat = [];
 D = spm_eeg_convert(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/spmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
-S.task = 'coregister';
-S.save = 1;
-D = spm_eeg_prep(S);
-
-
-S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/spmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
+S.D = ['spmeeg_', fname];
 S.timewin = [-3500 500];
 S.trialdef.conditionlabel = 'plan';
 S.trialdef.eventtype = 'STI101_up';
@@ -36,18 +35,17 @@ D = spm_eeg_epochs(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/espmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
+S.D = ['espmeeg_', fname];
 S.fsample_new = 250;
 S.prefix = 'd';
 D = spm_eeg_downsample(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/despmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
-S.mode = 'mark';
+S.D = ['despmeeg_', fname];
+S.mode = 'reject';
 S.badchanthresh = 0.2;
 S.methods.channels = {
-                      'all'
                       'MEGPLANAR'
                       }';
 S.methods.fun = 'flat';
@@ -59,7 +57,7 @@ D = spm_eeg_artefact(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/adespmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
+S.D = ['adespmeeg_', fname];
 S.channels = {'all'};
 S.frequencies = [8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45];
 S.timewin = [-Inf Inf];
@@ -73,14 +71,14 @@ D = spm_eeg_tf(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/tf_adespmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
+S.D = ['tf_adespmeeg_', fname];
 S.mode = 'replace';
 S.prefix = 'P';
 D = spm_eeg_combineplanar(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/Ptf_adespmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
+S.D = ['Ptf_adespmeeg_', fname];
 S.robust = false;
 S.circularise = false;
 S.prefix = 'm';
@@ -88,7 +86,7 @@ D = spm_eeg_average(S);
 
 
 S = [];
-S.D = '/media/mje/KINGSTON/MEG_libet/Data/mPtf_adespmeeg_sub_2_plan_tsss_mc_preproc_ica.mat';
+S.D = ['mPtf_adespmeeg_', fname];
 S.method = 'Rel';
 S.prefix = 'r';
 S.timewin = [-3500 -3200];
