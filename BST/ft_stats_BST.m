21aa3dc72ebadc18869ca04f58c6e082b9066bee
Mads Jensen <mje.mads@gmail.com>
2014-01-20 10:43:41 +0100

    added FT stats script

---
+++ BST/ft_stats_BST.m
@@ -0,0 +1,508 @@
+
+%%
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-3 .5];
+cfg.channel = {'MEGGRAD'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_GRAD_-3_05 stat
+
+%% 
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-3 .5];
+cfg.channel = {'MEGMAG'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_MAG_-3_05 stat
+
+%%
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-3 -2.35];
+cfg.channel = {'MEGMAG'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_MAG_-275_-235 stat
+
+
+%%
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+%load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-0.5 0];
+cfg.channel = {'MEGGRAD'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_GRAD_-05_-0 stat
+
+%%
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-0.5 0];
+cfg.channel = {'MEGMAG'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_MAG_-05_-0 stat
+
+%%
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+%load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-1.5 -0.5];
+cfg.channel = {'MEGGRAD'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_GRAD_-15_-05 stat
+%%
+clear;
+load GA_cls_cmb_re
+load GA_pln_cmb_re
+
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+%load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-1.5 -0.5];
+cfg.channel = {'MEGMAG'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_MAG_-15_-05 stat
+
+
+% %%
+clear;
+load GA_cls_cmb
+load GA_pln_cmb
+
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [0 .5];
+cfg.channel = {'MEGGRAD'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 12;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_GRAD_0_05 stat
+
+%% 
+clear;
+load GA_cls_cmb
+load GA_pln_cmb
+
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [0 .5];
+cfg.channel = {'MEGMAG'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_MAG_0_05 stat
+
+%%
+clear;
+load GA_cls_cmb
+load GA_pln_cmb
+
+% load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-1.5 -0.5];
+cfg.channel = {'MEGMAG'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_MAG_-150_-05 stat
+
+
+%%
+clear;
+load GA_cls_cmb
+load GA_pln_cmb
+
+load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306cmb_neighb.mat
+%load /users/mje/matlab/Toolboxes/Fieldtrip/template/neighbours/neuromag306mag_neighb.mat
+
+
+cfg = [];
+cfg.latency = [-.5 0];
+cfg.channel = {'MEGGRAD'};
+
+
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.correctm = 'cluster';
+cfg.clusteralpha = 0.05;
+cfg.clusterstatistic = 'maxsum';
+cfg.minnbchan = 2;
+cfg.neighbours = neighbours;
+cfg.tail = 0;
+cfg.clustertail = 0;
+cfg.alpha = 0.025;
+cfg.numrandomization = 5000;
+
+subj = 13;
+design = zeros(2,2*subj);
+for i = 1:subj
+  design(1,i) = i;
+end
+for i = 1:subj
+  design(1,subj+i) = i;
+end
+design(2,1:subj)        = 1;
+design(2,subj+1:2*subj) = 2;
+
+cfg.design = design;
+cfg.uvar  = 1;
+cfg.ivar  = 2;
+
+%
+[stat] = ft_timelockstatistics(cfg, GA_classic_cmb, GA_plan_cmb)
+
+save stat_GRAD_-05_0 stat
